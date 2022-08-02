721c1ccba2d3a2b86a4e65f33b6a2f3a02d998d1
Rodrigo Koshiyama <rodrigo.koshiyama@outlook.com>
2022-08-02 20:35:40 -0300

    first commit

--- Exemplo1 Table View/ViewController.swift
+++ Exemplo1 Table View/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
+    @IBOutlet weak var minhaTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        minhaTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        celula.textLabel?.text = "Teste..."
+        celula.imageView?.image = UIImage(named: "corinthians.png")
+        celula.detailTextLabel?.text = "blablabla"
+        celula.accessoryType = 
+        return celula
+    }
 }
 
